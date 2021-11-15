064aeb8ce8fd7e5849a7c5c459a332afdae2305f
anuj-kosambi <anujkosambi@gmail.com>
2021-11-16 01:47:13 +0530

    fix: adding cluster restoring path

--- data.tf
+++ data.tf
@@ -98,7 +98,8 @@ data "template_cloudinit_config" "k3s_server" {
         concat(
           ["server --cluster-init --node-taint CriticalAddonsOnly=true:NoExecute --write-kubeconfig-mode 644 --node-label unravel.node.kubernetes.io/role=master"],
           var.k3s_server_backup != null && var.k3s_server_backup.path != null && var.enable_backup_server ? [
-            "--etcd-s3 --cluster-reset --etcd-s3-region=${var.aws_region} --etcd-s3-folder=${var.name} --etcd-s3-bucket=${var.k3s_server_backup.path} --etcd-s3-access-key=${var.k3s_server_backup.id} --etcd-s3-secret-key=${var.k3s_server_backup.secret}"
+            "--etcd-s3 --cluster-reset --cluster-reset-restore-path=${var.k3s_server_backup.path}"
+            //"--etcd-s3 --cluster-reset --etcd-s3-region=${var.aws_region} --etcd-s3-folder=${var.name} --etcd-s3-bucket=${var.k3s_server_backup.path} --etcd-s3-access-key=${var.k3s_server_backup.id} --etcd-s3-secret-key=${var.k3s_server_backup.secret}"
       ] : []))
     })
   }
