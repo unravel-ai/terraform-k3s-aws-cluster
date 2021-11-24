43d7e78a3150ca388e06889f91d5d35d44f55f6e
anuj-kosambi <anujkosambi@gmail.com>
2021-11-24 22:35:21 +0530

    chore: updating k3s from 1.22.4 from 1.21.3

--- variables.tf
+++ variables.tf
@@ -137,7 +137,7 @@ variable "private_subnets" {
 }
 
 variable "install_k3s_version" {
-  default     = "1.21.4+k3s1"
+  default     = "1.22.3+k3s2"
   type        = string
   description = "Version of K3S to install"
 }
